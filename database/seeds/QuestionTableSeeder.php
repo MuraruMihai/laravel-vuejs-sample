caeee44f57cc07170a0cd672662dce78989c3aa3
Muraru Mihai <mihai.mtz007@gmail.com>
2020-08-18 23:35:50 +0300

    make db connection create models create migrations

---
+++ database/seeds/QuestionTableSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class QuestionTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+    }
+}
