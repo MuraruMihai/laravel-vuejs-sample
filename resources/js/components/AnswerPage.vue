731d7faa2fdf22121e29a63716a8123a1f046bd9
Muraru Mihai <mihai.mtz007@gmail.com>
2020-08-21 01:14:15 +0300

    add some design elements

--- resources/js/components/AnswerPage.vue
+++ resources/js/components/AnswerPage.vue
@@ -1,12 +1,24 @@
 <template>
     <div class="container">
-        <h1>Answers</h1>
+        <h3>Answers</h3>
         <div v-for="answer in response">
             <label>{{ answer.choice }} {{ answer.percentage }}% - {{ answer.number }} results </label>
         </div>
 
     </div>
 </template>
+<style lang="css" scoped>
+    .container{
+        margin-top: 75px;
+        border: 1px solid #abb3c6;
+        padding: 30px;
+    }
+    h3{
+        text-decoration: underline;
+        text-decoration-color: #6db8b8;
+        font-family: roboto;
+    }
+</style>
 <script>
 export default {
     data: function (){
